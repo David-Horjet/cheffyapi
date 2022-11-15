9b8bc3772ef968b168bbfe0f27378ef29ed2dfb6
David Horjet <davidojetola956@gmail.com>
2022-11-15 17:56:43 +0100

    make it better

--- server.js
+++ server.js
@@ -37,7 +37,93 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', async (req, res) => {
-     const emailToSend = path.join(__dirname+'./public/template.html');
+     const emailToSend = `
+     <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: 0;
+      list-style: none;
+      font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+        "Lucida Sans", Arial, sans-serif;
+    }
+
+    hr {
+      margin: 10px 0;
+    }
+
+    body {
+      background-color: rgb(240, 240, 240);
+    }
+
+    .wrapper {
+      margin: 50px;
+      padding: 30px 50px;
+      background-color: rgb(22, 21, 41);
+      color: #ffffff;
+    }
+
+    .wrapper h2 {
+      color: #00ff00;
+    }
+
+    .wrapper h1 span {
+      color: #00ff00;
+    }
+
+    @media (max-width: 560px) {
+      .wrapper {
+        margin: 20px;
+        padding: 20px;
+      }
+
+      .wrapper h1 {
+        font-size: 30px;
+      }
+
+      .wrapper h2 {
+        font-size: 20px;
+      }
+    }
+  </style>
+  <body>
+    <div class="wrapper">
+      <div class="image">
+        <img src="" alt="" />
+      </div>
+      <div class="content">
+        <h1>Hi <span>Co-</span>creatives,</h1>
+        <hr />
+        <p>You have a new contact request from your website</p>
+        <br />
+        <h2>Contact Details</h2>
+        <br />
+        <ul>
+          <li><strong>Name</strong>: ${req.body.name}</li>
+          <br />
+          <li><strong>Email</strong>: ${req.body.email}</li>
+          <br />
+          <li><strong>Subject</strong>: ${req.body.subject}</li>
+          <br />
+        </ul>
+        <h2>Contact Message</h2>
+        <br />
+        <p>${req.body.message}</p>
+      </div>
+    </div>
+  </body>
+</html>
+
+     `;
 
      // create reusable transporter object using the default SMTP transport
      let transporter = nodemailer.createTransport({
@@ -50,7 +136,7 @@ app.post('/', async (req, res) => {
 
      // send mail with defined transport object
      let mailOptions = {
-          from: req.body.email,// sender address
+          from: req.body.email, // sender address
           to: EMAIL, // list of receivers
           subject: req.body.subject, // Subject line
           text: "Co-creatives has sent you a contact message", // plain text body
